cfb356f3de7bec0a3ef44d1f64162f9329775845
Pannawit24 <145045844+Pannawit24@users.noreply.github.com>
2025-10-21 01:59:52 +0700

    Update Code action_update.php insert update and delete ( Products )

--- include/action_update.php
+++ include/action_update.php
@@ -4,41 +4,71 @@ include_once 'connDB.php';
 print_r($_POST);
 
 $tbName = $_POST['tb_name'];
-$tbName = $_POST['action'];
+$action = $_POST['action'];
 
 switch ($tbName) {
     case 'tb_product':
         switch ($action) {
+
             case 'insert':
-                //code
+                $sql = "INSERT INTO tb_products (c_ProductName, i_SupplierID, i_CategoryID, c_Unit, i_Price) 
+                        VALUES (:param_pname, :param_supid, :param_catid, :param_unit, :param_price)";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':param_pname', $_POST['c_ProductName'], PDO::PARAM_STR);
+                $stmt->bindValue(':param_supid', $_POST['i_SupplierID'], PDO::PARAM_INT);
+                $stmt->bindValue(':param_catid', $_POST['i_CategoryID'], PDO::PARAM_INT);
+                $stmt->bindValue(':param_unit', $_POST['c_Unit'], PDO::PARAM_STR);
+                $stmt->bindValue(':param_price', $_POST['i_Price'], PDO::PARAM_INT);
+
+                try {
+                    $stmt->execute();
+                    echo "New record created successfully";
+                    header("Location: ../db_product_search.php");
+                } catch (PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
                 break;
+
             case 'update':
-                //code
-                break;
-        }
-        //code
-        $sql = "UPDATE tb_products SET 
+                $sql = "UPDATE tb_products SET 
                     c_ProductName = :param_pname , 
                     i_SupplierID  = :param_supid , 
                     i_CategoryID  = :param_catid , 
                     c_Unit        = :param_unit  ,
                     i_Price       = :param_price
                 WHERE tb_products.i_ProductID = :param_pid";
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':param_pname', $_POST['c_ProductName'], PDO::PARAM_STR);
-        $stmt->bindValue(':param_supid', $_POST['i_SupplierID'], PDO::PARAM_INT);
-        $stmt->bindValue(':param_catid', $_POST['i_CategoryID'], PDO::PARAM_INT);
-        $stmt->bindValue(':param_unit', $_POST['c_Unit'], PDO::PARAM_STR);
-        $stmt->bindValue(':param_price', $_POST['i_Price'], PDO::PARAM_INT);
-        $stmt->bindValue(':param_pid', $_POST['i_ProductID'], PDO::PARAM_INT);
-        
-        try{
-            $stmt->execute();
-            echo $stmt->rowCount() . " records UPDATED successfully";
-                header("Location: ../db_product_search.php");
-        } catch(PDOException $e){
-            echo "Error: " . $e->getMessage();
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':param_pname', $_POST['c_ProductName'], PDO::PARAM_STR);
+                $stmt->bindValue(':param_supid', $_POST['i_SupplierID'], PDO::PARAM_INT);
+                $stmt->bindValue(':param_catid', $_POST['i_CategoryID'], PDO::PARAM_INT);
+                $stmt->bindValue(':param_unit', $_POST['c_Unit'], PDO::PARAM_STR);
+                $stmt->bindValue(':param_price', $_POST['i_Price'], PDO::PARAM_INT);
+                $stmt->bindValue(':param_pid', $_POST['i_ProductID'], PDO::PARAM_INT);
+
+                try {
+                    $stmt->execute();
+                    echo $stmt->rowCount() . " records UPDATED successfully";
+                    header("Location: ../db_product_search.php");
+                } catch (PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+                break;
+                
+            case 'delete':
+                $sql = "DELETE FROM tb_products WHERE i_ProductID = :param_pid";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':param_pid', $_POST['i_ProductID'], PDO::PARAM_INT);
+                
+                try{
+                    $stmt->execute();
+                    echo $stmt->rowCount() . " records DELETED successfully";
+                    header("Location: ../db_product_search.php");
+                } catch(PDOException $e){
+                    echo "Error: " . $e->getMessage();
+                }
+                break;
         }
+        //code
         break;
     default:
         //code block
